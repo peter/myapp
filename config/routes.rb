c0db58b0f682bc062aa0bf565801df23ae644a5c
Peter Marklund <peter@marklunds.com>
2012-06-30 23:35:13 +0200

    root to: 'posts#index'

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root to: 'welcome#index'
+  root to: 'posts#index'
 
   # Sample of regular route:
   #   get 'products/:id' => 'catalog#view'
